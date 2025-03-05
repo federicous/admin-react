7923fea798d983e2ead5e97069010951f565b271
Fede <federicous@gmail.com>
2025-03-05 18:59:43 -0300

    select provincia localidad

--- src/components/Formularios/ClientesEdit.jsx
+++ src/components/Formularios/ClientesEdit.jsx
@@ -6,90 +6,289 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import SaveIcon from '@mui/icons-material/Save';
 import CancelIcon from '@mui/icons-material/Close';
+import dayjs from 'dayjs';
+import "dayjs/locale/es";
 import {
   GridRowModes,
   DataGrid,
   GridToolbarContainer,
   GridActionsCellItem,
   GridRowEditStopReasons,
+  GridToolbar,
+  GridToolbarExport,
+  GridToolbarColumnsButton,
+  GridToolbarDensitySelector,
+  GridToolbarQuickFilter,
+  GridToolbarFilterButton,
 } from '@mui/x-data-grid';
-import {
-  randomCreatedDate,
-  randomTraderName,
-  randomId,
-  randomArrayItem,
-} from '@mui/x-data-grid-generator';
+import { esES } from '@mui/x-data-grid/locales';
+import {provincias} from "../utils/provincias"
+import {localidades} from "../utils/localidades"
+// import {
+//   randomCreatedDate,
+//   randomTraderName,
+//   randomId,
+//   randomArrayItem,
+// } from '@mui/x-data-grid-generator';
 
-const roles = ['Market', 'Finance', 'Development'];
-const randomRole = () => {
-  return randomArrayItem(roles);
-};
+import ApiQuery from "../utils/apiQuery/apiQuery";
 
-const initialRows = [
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 25,
-    joinDate: randomCreatedDate(),
-    role: randomRole(),
-  },
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 36,
-    joinDate: randomCreatedDate(),
-    role: randomRole(),
-  },
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 19,
-    joinDate: randomCreatedDate(),
-    role: randomRole(),
-  },
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 28,
-    joinDate: randomCreatedDate(),
-    role: randomRole(),
-  },
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 23,
-    joinDate: randomCreatedDate(),
-    role: randomRole(),
-  },
-];
+let apiQuery = new ApiQuery();
+
+// const roles = ['Market', 'Finance', 'Development'];
+// const randomRole = () => {
+//   return ["asd","pepe"];
+// };
+// const randomRole = () => {
+// 	const randomIndex = Math.floor(Math.random() * roles.length);
+// 	return roles[randomIndex];
+//       }
+
+// const initialRows = [
+//   {
+//     id: 1111,
+//     name: "pepe",
+//     age: 25,
+//     joinDate: dayjs(),
+//     role: randomRole(),
+//   },
+//   {
+//     id: 2222,
+//     name: "jose",
+//     age: 36,
+//     joinDate: dayjs(),
+//     role: randomRole(),
+//   },
+//   {
+//     id: 3333,
+//     name: "juan",
+//     age: 19,
+//     joinDate: dayjs(),
+//     role: randomRole(),
+//   },
+// ];
+
+function compararObjetos(obj1, obj2) {
+  // const diferencias = {};
+  // // Combina las claves de ambos objetos para iterar sobre todas las propiedades
+  // const todasLasClaves = new Set([...Object.keys(objeto1), ...Object.keys(objeto2)]);
+
+  // todasLasClaves.forEach(clave => {
+  //   if (objeto1[clave] !== objeto2[clave]) {
+  //     diferencias[clave] = objeto2[clave]; // Toma el valor del objeto2
+  //   }
+  // });
+  // return diferencias;
+
+  let resultado = {};
+
+  Object.keys(obj2).forEach(clave => {
+      if (obj1[clave] !== obj2[clave]) {
+          resultado[clave] = obj2[clave];
+      }
+  });
+
+  return resultado;
+}
+
+function generarNumeroAleatorio(cantidadDeCifras) {
+  const minimo = Math.pow(10, cantidadDeCifras - 1); // Calcula el valor mínimo (ej: 100 para 3 cifras)
+  const maximo = Math.pow(10, cantidadDeCifras) - 1; // Calcula el valor máximo (ej: 999 para 3 cifras)
+  return Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+}
 
 function EditToolbar(props) {
   const { setRows, setRowModesModel } = props;
 
   const handleClick = () => {
-    const id = randomId();
+    const _id = generarNumeroAleatorio(6);
     setRows((oldRows) => [
       ...oldRows,
-      { id, name: '', age: '', role: '', isNew: true },
+      { _id, name: '', email: '', ferreteria: '',vendedor: '',localidad: '',provincia:'',address:'',cuit:'',phone:'',password:'', isNew: true },
     ]);
     setRowModesModel((oldModel) => ({
       ...oldModel,
-      [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
+      [_id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
     }));
   };
 
   return (
     <GridToolbarContainer>
-      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+	<div style={{ width: '100%' }}>
+	<Box
+	 sx={{   
+	display: "flex",
+	flexDirection: "row",
+	justifyContent: 'space-between'
+}} 
+	>
+	<Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+        Agregar usuario
+      </Button>
+      <GridToolbar />
+      {/* <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
         Add record
       </Button>
+      <GridToolbar /> */}
+      <GridToolbarQuickFilter />
+      </Box>
+      </div>
     </GridToolbarContainer>
   );
 }
 
 export default function FullFeaturedCrudGrid() {
-  const [rows, setRows] = React.useState(initialRows);
+//   const [rows, setRows] = React.useState(initialRows);
+  const [rows, setRows] = React.useState([]);
   const [rowModesModel, setRowModesModel] = React.useState({});
+  const [open, setOpen] = React.useState(false);
+  const [eliminar, setEliminar] = React.useState(false);
+  const [isAdmin, setIsAdmin] = React.useState(false);
+  const [userPermissions, setUserPermissions] = React.useState([]);
+  const [userAccesos, setUserAccesos] = React.useState([]);
+  const [listaClientes, setListaClientes] = React.useState([])
+  const [localidadArray, setLocalidadArray] = React.useState([]);
+  const [provincia, setProvincia] = React.useState('');
+
+  const handleClose = () => {
+  setOpen(false);
+  };
+
+  function capitalizeFirstLetter(string) {
+    let cadena = string.toLowerCase()
+    return cadena.charAt(0).toUpperCase() + cadena.slice(1);
+  }
+
+  const getLocalidadesOptions = (provincia) => {
+    let array = localidades.filter((item)=> item.provincia.nombre.match(new RegExp(`${provincia}`,'gi'))).map(objeto => objeto.nombre)
+    // console.log(provincia);    
+    // console.log(array);    
+    let uniq = [...new Set(array)];
+    // console.log(uniq);    
+    // setLocalidadArray(uniq)
+    return uniq
+    // return provincia
+    //   ? localidades
+    //       .filter((loc) => loc.provincia === provincia)
+    //       .map((loc) => loc.nombre)
+    //   : [];
+  };
+
+	const columnsBloqueados = [
+		{ field: 'name', headerName: 'Cliente', width: 130, flex:0.8, minWidth:110, editable: true, },
+		{ field: 'email', headerName: 'Email', width: 230, flex:1.5, minWidth:110, editable: true, },
+		// { field: 'fecha', headerName: 'Fecha', width: 130, flex:0.8, minWidth:110, type:"date", valueGetter: ({value}) => value && dayjs(value,'DD/MM/YYYY').toDate(), },
+		{ field: 'ferreteria', headerName: 'Ferreteria', width: 130, flex:0.8, minWidth:110, editable: true, },
+		{ field: 'vendedor', headerName: 'Vendedor', width: 130, flex:0.8 , minWidth:110, editable: true,
+      type: 'singleSelect',
+      valueOptions: ['Leonel', 'Moises'],
+      valueGetter: (params) => params.row.vendedor ? capitalizeFirstLetter(params.row.vendedor) : ""
+    },
+		{ field: 'provincia', headerName: 'Provincia', width: 130, flex:0.8 , minWidth:110, editable: true,
+      type: 'singleSelect',
+      valueOptions: provincias.sort(function (a, b) {
+        if (a.iso_nombre > b.iso_nombre) {
+          return 1;
+        }
+        if (a.iso_nombre < b.iso_nombre) {
+          return -1;
+        }
+        return 0;
+      }).map(objeto => objeto.iso_nombre),
+      valueGetter: (params) => params.row.provincia ?? "",
+    },
+    { field: 'localidad', headerName: 'Localidad', width: 130, flex:0.8 , minWidth:110, /* editable: true, */
+      valueGetter: (params) => params.row.localidad? params.row.localidad.toUpperCase() : "",
+      type: 'singleSelect',
+      valueOptions: (params) => getLocalidadesOptions(params.row.provincia).sort(function (a, b) {
+          if (a > b) { return 1; }
+          if (a < b) { return -1; }
+          // a must be equal to b
+          return 0;
+        }),
+      editable: (params) => Boolean(params.row.provincia), // Deshabilita si no hay provincia seleccionada
+    },
+		{ field: 'address', headerName: 'Dirección', width: 130, flex:0.8 , minWidth:110, editable: true,},
+		{ field: 'cuit', headerName: 'Cuit', width: 130, flex:0.8 , minWidth:110, editable: true, type: "number"},
+		{ field: 'phone', headerName: 'Tel', width: 130, flex:0.8 , minWidth:110, editable: true, type: "number"},
+		{ field: 'descuento', headerName: 'Desc[%]', width: 130, flex:0.8 , minWidth:110, editable: true, type: "number", headerAlign:'center', align:'center'},
+		{ field: 'isAdmin', headerName: 'Admin', width: 130, flex:0.8 , minWidth:110, editable: true, headerAlign:'center', align:'center'},
+		{ field: 'password', headerName: 'Clave', type: 'password', width: 130, flex:0.8 , minWidth:110, renderCell:(params) => ("**********"), editable: true, },
+		{
+			field: 'actions',
+			type: 'actions',
+			headerName: 'Acciones',
+			width: 100,
+			cellClassName: 'actions',
+			getActions: ({ id }) => {
+				// console.log(id);
+				
+				const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+				if (isInEditMode) {
+				return [
+				<GridActionsCellItem
+					icon={<SaveIcon />}
+					label="Save"
+					sx={{
+						color: 'primary.main',
+					}}
+					onClick={handleSaveClick(id)}
+				/>,
+				<GridActionsCellItem
+					icon={<CancelIcon />}
+					label="Cancel"
+					className="textPrimary"
+					onClick={handleCancelClick(id)}
+					color="inherit"
+				/>,
+				];
+				}
+
+				return [
+				<GridActionsCellItem
+					icon={<EditIcon />}
+					label="Edit"
+					className="textPrimary"
+					onClick={handleEditClick(id)}
+					color="inherit"
+				/>,
+				<GridActionsCellItem
+				icon={<DeleteIcon />}
+				label="Delete"
+				onClick={handleDeleteClick(id)}
+				color="inherit"
+				/>,
+				];
+		},
+		},
+		];
+	const columnsBloqueadosAdmin = [...columnsBloqueados,
+		// { field: 'remove', headerName: 'Eliminar', width: 130, flex:0.8, headerAlign:'center', align:'center', renderCell:(params) => (		
+		// 	<Link sx={{cursor:"pointer", justifyContent:"center"}} onClick={() => removeElement(params.row._id)}>
+		// 		<Button color='error' variant='contained' sx={{minWidth:"fit-content", p:"6px"}}>
+		// 			<Delete />
+		// 		</Button>
+		// 	</Link>
+		// ) },
+	]
+
+  React.useEffect(() => {
+	let cancel = false;
+	    setOpen(true);
+	apiQuery.get(`/api/users/`)
+	.then((res) => {
+		if (cancel) return;
+		setListaClientes(res);
+		setRows(res)
+		// console.log(res);		
+		setOpen(false);
+	})
+	return () => { 
+		  cancel = true;
+		      setOpen(false);
+	}
+      }, [eliminar])
 
   const handleRowEditStop = (params, event) => {
     if (params.reason === GridRowEditStopReasons.rowFocusOut) {
@@ -98,15 +297,22 @@ export default function FullFeaturedCrudGrid() {
   };
 
   const handleEditClick = (id) => () => {
+	console.log(id);
+	console.log(rowModesModel);
     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
   };
 
   const handleSaveClick = (id) => () => {
     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    console.log(rowModesModel);
+    console.log(GridRowModes.View);
+	  console.log([id]);    
   };
 
   const handleDeleteClick = (id) => () => {
-    setRows(rows.filter((row) => row.id !== id));
+    console.log(rows.find((row) => row._id === id));
+    const deletedRow = rows.find((row) => row._id === id)
+    setRows(rows.filter((row) => row._id !== id));
   };
 
   const handleCancelClick = (id) => () => {
@@ -115,15 +321,28 @@ export default function FullFeaturedCrudGrid() {
       [id]: { mode: GridRowModes.View, ignoreModifications: true },
     });
 
-    const editedRow = rows.find((row) => row.id === id);
+    const editedRow = rows.find((row) => row._id === id);
     if (editedRow.isNew) {
-      setRows(rows.filter((row) => row.id !== id));
+      setRows(rows.filter((row) => row._id !== id));
     }
   };
 
   const processRowUpdate = (newRow) => {
     const updatedRow = { ...newRow, isNew: false };
-    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    console.log(rows);
+    setRows(rows.map((row) => (row._id === newRow._id ? updatedRow : row)));
+    console.log(rows);
+    console.log(updatedRow);
+    if (updatedRow.name && updatedRow.email && updatedRow.password && updatedRow.address && updatedRow.cuit && updatedRow.ferreteria && updatedRow.phone && updatedRow.provincia && updatedRow.localidad && updatedRow.vendedor) {
+      console.log(`COMPLETO`);      
+    } else {
+      console.log(`FALTAN`);      
+    }
+    let filaOriginal = rows.filter((row) => row._id == updatedRow._id);
+    const diferencias = compararObjetos(filaOriginal[0], updatedRow);
+    diferencias._id = updatedRow._id
+    console.log(diferencias);
+    
     return updatedRow;
   };
 
@@ -131,79 +350,81 @@ export default function FullFeaturedCrudGrid() {
     setRowModesModel(newRowModesModel);
   };
 
-  const columns = [
-    { field: 'name', headerName: 'Name', width: 180, editable: true },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 80,
-      align: 'left',
-      headerAlign: 'left',
-      editable: true,
-    },
-    {
-      field: 'joinDate',
-      headerName: 'Join date',
-      type: 'date',
-      width: 180,
-      editable: true,
-    },
-    {
-      field: 'role',
-      headerName: 'Department',
-      width: 220,
-      editable: true,
-      type: 'singleSelect',
-      valueOptions: ['Market', 'Finance', 'Development'],
-    },
-    {
-      field: 'actions',
-      type: 'actions',
-      headerName: 'Actions',
-      width: 100,
-      cellClassName: 'actions',
-      getActions: ({ id }) => {
-        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+//   const columns = [
+//     { field: 'name', headerName: 'Name', width: 180, editable: true },
+//     {
+//       field: 'age',
+//       headerName: 'Age',
+//       type: 'number',
+//       width: 80,
+//       align: 'left',
+//       headerAlign: 'left',
+//       editable: true,
+//     },
+//     {
+//       field: 'joinDate',
+//       headerName: 'Join date',
+// //       type: 'date',
+//       width: 180,
+//       editable: true,
+//     },
+//     {
+//       field: 'role',
+//       headerName: 'Department',
+//       width: 220,
+//       editable: true,
+//       type: 'singleSelect',
+//       valueOptions: ['Market', 'Finance', 'Development'],
+//     },
+//     {
+//       field: 'actions',
+//       type: 'actions',
+//       headerName: 'Actions',
+//       width: 100,
+//       cellClassName: 'actions',
+//       getActions: ({ id }) => {
+// 	console.log(id);
+	
+//         const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
 
-        if (isInEditMode) {
-          return [
-            <GridActionsCellItem
-              icon={<SaveIcon />}
-              label="Save"
-              sx={{
-                color: 'primary.main',
-              }}
-              onClick={handleSaveClick(id)}
-            />,
-            <GridActionsCellItem
-              icon={<CancelIcon />}
-              label="Cancel"
-              className="textPrimary"
-              onClick={handleCancelClick(id)}
-              color="inherit"
-            />,
-          ];
-        }
+//         if (isInEditMode) {
+//           return [
+//             <GridActionsCellItem
+//               icon={<SaveIcon />}
+//               label="Save"
+//               sx={{
+//                 color: 'primary.main',
+//               }}
+//               onClick={handleSaveClick(id)}
+//             />,
+//             <GridActionsCellItem
+//               icon={<CancelIcon />}
+//               label="Cancel"
+//               className="textPrimary"
+//               onClick={handleCancelClick(id)}
+//               color="inherit"
+//             />,
+//           ];
+//         }
 
-        return [
-          <GridActionsCellItem
-            icon={<EditIcon />}
-            label="Edit"
-            className="textPrimary"
-            onClick={handleEditClick(id)}
-            color="inherit"
-          />,
-          <GridActionsCellItem
-            icon={<DeleteIcon />}
-            label="Delete"
-            onClick={handleDeleteClick(id)}
-            color="inherit"
-          />,
-        ];
-      },
-    },
-  ];
+//         return [
+//           <GridActionsCellItem
+//             icon={<EditIcon />}
+//             label="Edit"
+//             className="textPrimary"
+//             onClick={handleEditClick(id)}
+//             color="inherit"
+//           />,
+//           <GridActionsCellItem
+//             icon={<DeleteIcon />}
+//             label="Delete"
+//             onClick={handleDeleteClick(id)}
+//             color="inherit"
+//           />,
+//         ];
+//       },
+//     },
+//   ];
 
   return (
     <Box
@@ -220,7 +441,9 @@ export default function FullFeaturedCrudGrid() {
     >
       <DataGrid
         rows={rows}
-        columns={columns}
+	getRowId={(row) => row._id} 
+        // columns={columns}
+	columns={(isAdmin || userAccesos.includes("desbloqueoSender")) ? columnsBloqueadosAdmin : columnsBloqueados}
         editMode="row"
         rowModesModel={rowModesModel}
         onRowModesModelChange={handleRowModesModelChange}
@@ -228,8 +451,16 @@ export default function FullFeaturedCrudGrid() {
         processRowUpdate={processRowUpdate}
         slots={{ toolbar: EditToolbar }}
         slotProps={{
-          toolbar: { setRows, setRowModesModel },
+          toolbar: { setRows, setRowModesModel, showQuickFilter: true, },
         }}
+	// slots={{toolbar: GridToolbar}} 
+	// slotProps={{toolbar: {showQuickFilter: true,},}}
+	localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+	initialState={{
+		sorting: {
+		  sortModel: [{ field: 'estado', sort: 'desc' }],
+		},
+	      }}
       />
     </Box>
   );
